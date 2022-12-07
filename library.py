eed7b47e6358b2f2e880ed5b7417ae7f1baa4e5b
Hoàng Quang Nhân <81468391+blackfox20092006@users.noreply.github.com>
2022-12-07 20:26:01 +0700

    Update library.py

--- library.py
+++ library.py
@@ -1,31 +1,77 @@
-#sử dụng đường dẫn thư mục khi gọi hàm, vui lòng sử dụng theo định dạng sau : ví dụ : C:\\Users\\DELL
 import os
 main_path = os.getcwd()
 database_path = main_path + '\\' + 'database'
-def fList(path): #return danh sách các file ở trong <path>
+def fCheckdb():
+    try:
+        os.chdir(database_path)
+        os.chdir(main_path)
+        return 0
+    except:
+        return 'MissingDatabase'
+def fList(path, mode):
+    mode = int(mode)
     f_list = os.listdir(path)
     f_list2 = []
     for i in range(len(f_list)):
         if len(f_list[i].split('.')) == 2 and (f_list[i].split('.'))[0] != '':
             f_list2.append(f_list[i])
-    return f_list2
-def fFilter(data, tag): #lọc các file theo tag trong  thư mục database
+    if mode == 1:
+        return f_list2
+    elif mode == 2:
+        rvalue = {}
+        rvalue2 = []
+        for i in range(len(f_list2)):
+            rvalue = {}
+            rvalue['num'] = i+1
+            rvalue['filename'] = f_list2[i]
+            rvalue['type'] = (f_list2[i].split('.'))[len(f_list2[i].split('.'))-1] + ' File'
+            rvalue2 += [rvalue]
+        return rvalue2
+    else:
+        return 'InvaildModeParam'
+def fFilter(data, tag, mode):
+    mode = int(mode)
     data = list(data)
     os.chdir(database_path)
-    temp = []
-    key = tag + '__'
-    for i in range(len(data)):
-        if data[i].find(key) != -1:
-            temp += [data[i]]
-    return temp
+    if mode == 1:
+        temp = []
+        key = tag + '__'
+        for i in range(len(data)):
+            if data[i].find(key) != -1:
+                temp += [data[i]]
+        return temp
+    elif mode == 2:
+        temp = []
+        key = tag + '__'
+        for i in range(len(data)):
+            if data[i].find(key) != -1:
+                temp += [data[i]]
+        rvalue = {}
+        rvalue2 = []
+        for i in range(len(temp)):
+            rvalue = {}
+            rvalue['num'] = i+1
+            rvalue['filename'] = temp[i]
+            rvalue['type'] = (temp[i].split('.'))[len(temp[i].split('.'))-1] + ' File'
+            rvalue2 += [rvalue]
+        return rvalue2
+    else:
+        return 'InvailModeParam'
 def fAdd_tag(old_name, tag):
-    #exitcode = 0 => add tag to file successfully
-    #exitcode = -1 => add tag to file unsuccessfully
     os.chdir(database_path)
-    if fList(database_path).find(old_name) != -1:
+    if fChecktag(tag) == -1:
+        return 1
+    if str(fList(database_path, 1)).find(old_name) != -1:
+        if len(old_name.split('__')) > 1:
+            return 2
+        while old_name.find('__') != -1:
+            old_name = old_name.replace('__', '_')
         new_name = tag + '__' + old_name
-        os.rename(old_name, new_name)
-        return 0
+        try:
+            os.rename(old_name, new_name)
+            return 0
+        except:
+            return -1
     else:
         return -1
 def fChecktag(tag):
@@ -34,60 +80,17 @@ def fChecktag(tag):
             return -1
     else:
         return 0
-def fChangetag(new_tag, file_name): #exicode = -1 => invaild tag
-    #exitcode = -2 => exist tag
-    #exitcode = 0 => change tag successfully
-    #exitcode = 1 => change tag unsuccessfully => not found filename or do not have premission
+def fChangetag(new_tag, file_name):
     if fChecktag(new_tag) == -1:
         return -1
     elif new_tag == (file_name.split('__'))[0]:
         return -2
     else:
         os.chdir(database_path)
-        filelist = fList(database_path)
+        filelist = fList(database_path, 1)
         for i in range(len(filelist)):
             if filelist[i] == file_name:
                 t = filelist[i].split('__')
                 ans = new_tag + '__' + t[1]
                 os.rename(file_name, ans)
                 return 0
-def fAddtags (list_file, begin, end, tag): #exitcode = -2 => tag is not allow
-    #exitcode = 0 => add tags successfully
-    #exitcode = -3 => invail param for end
-    #exitcode = 1 => file has already had tag, use fChangetags to change tag for many files
-    begin = int(begin)
-    end = int(end)
-    list_file = list(list_file)
-    os.chdir(database_path)
-    if fChecktag(tag) == -1:
-        return -2
-    elif end > len(list_file) or end < begin:
-        return -3
-    else:
-        for i in range(begin-1, end, 1):
-            if len(list_file[i].split('__')) != 1:
-                return 1
-            else:
-                temp = list_file[i].split('.')
-                newname = tag + '__' + temp[0] + '.' +temp[1]
-                os.rename(list_file[i], newname)
-    return 0
-def fChangetags(list_file, begin, end, tag, new_tag): #change from begin-th file to end-th file with new tag
-    # exitcode = -2 => tag is not allow
-    #exitcode = -3 => invail param for end
-    #exitcode = 0 => successfully rename
-    #list_file is a list which contains all files in database directory, this function will automatically filt the specific tag
-    begin = int(begin)
-    end = int(end)
-    list_file = list(list_file)
-    if fChecktag(tag) == -1:
-        return -1
-    elif end > len(list_file) or end < begin:
-        return -3
-    else:
-        os.chdir(database_path)
-        data = fFilter(list_file, tag)
-        for i in range(begin, end+1):
-            s = new_tag + '__' + (data[i].split('__'))[1]
-            os.rename(data[i], s)
-        return 0
